3cc67fe13a939fa63b8c91dbcd19b02594e03631
Ilqar03 <tu757r7k9@code.edu.az>
2022-11-03 11:07:22 +0400

    Alqoritmlerin helli

---
+++ C#/tap 1.cs
@@ -0,0 +1,44 @@
+﻿
+//int a = 56;
+//int b = 76;
+//int n = 50;
+//int c = 0;
+
+
+//if (a > 50 && b > 50)
+//{
+//    c = a + b;
+//    Console.WriteLine("ededlerin cemi" + c);
+//}
+//else
+//{
+//    Console.WriteLine("ededler 50 den kicikdir");
+//}
+
+
+
+
+//int a = Convert.ToInt32(Console.ReadLine());
+//int b = Convert.ToInt32(Console.ReadLine());
+//int n = 50;
+//int c = 0;
+
+
+//if (a > 50 && b > 50)
+//{
+//    c = a + b;
+//    Console.WriteLine("ededlerin cemi" + c);
+//}
+//else
+//{
+//    Console.WriteLine("ededler 50 den kicikdir");
+//}
+
+
+
+
+
+
+
+
+
